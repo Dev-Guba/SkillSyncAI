1039e0c29b23e8e8b301f52dc6efe86a6dfa4ca9
Dev-Guba <jamesdavidguba2@gmail.com>
2026-07-29 16:56:39 +0800

    Updated

--- AI/main.py
+++ AI/main.py
@@ -30,11 +30,6 @@ def analyze(data: dict):
     user_skills = data["user_skills"]
     required_skills = data["required_skills"]
 
-    print({
-        "user_skills": user_skills,
-        "required_skills": required_skills
-    })
-
     result = calculate_skill_gap(
         user_skills,
         required_skills
